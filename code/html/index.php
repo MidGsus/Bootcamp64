094d0fdc7e42d3ff0ecfc1050b48674249f55e5a
MidGsus <55570667+MidGsus@users.noreply.github.com>
2019-09-29 21:45:23 -0700

    Se termino la insercion de nombres en la base de datos

--- code/html/index.php
+++ code/html/index.php
@@ -2,7 +2,8 @@
 
 require 'core/bootstrap.php';
 
-$request = $_SERVER['REQUEST_URI'];
+//$request = $_SERVER['REQUEST_URI'];
+$request =parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch ($request) {
     case '/' :
@@ -19,6 +20,9 @@ switch ($request) {
         break;
     case '/contact' :
         require __DIR__ . '/controllers/contact.php';
+        break;
+    case '/names' :
+        require __DIR__ . '/controllers/add-name.php';
         break;
     default:
         require __DIR__ . '/views/404.view.php';
